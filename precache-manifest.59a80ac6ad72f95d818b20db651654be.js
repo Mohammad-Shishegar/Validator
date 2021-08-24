15ff7206abb89c80c14458172806ad79c251797d
Mohammad_Shishegar <mohammad127sh82@gmail.com>
2021-08-24 11:09:01 +0430

    Updates

---
+++ precache-manifest.59a80ac6ad72f95d818b20db651654be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae426acc92970fd58a21bfd7f0d63545",
+    "url": "/Validator/index.html"
+  },
+  {
+    "revision": "a4c8f5cec4feeb504747",
+    "url": "/Validator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e46b3caa56fc779f82a",
+    "url": "/Validator/static/js/2.4976fbc9.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/Validator/static/js/2.4976fbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c8f5cec4feeb504747",
+    "url": "/Validator/static/js/main.d7b7d042.chunk.js"
+  },
+  {
+    "revision": "4d9d2fbb373fd9eafcdb",
+    "url": "/Validator/static/js/runtime-main.4512c825.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Validator/static/media/logo.5d5d9eef.svg"
+  }
+]);
